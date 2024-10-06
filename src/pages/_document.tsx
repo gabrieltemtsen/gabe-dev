c71c9ec23fb2e912f41fd33ef899a7865a3173ff
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-10-06 19:12:49 +0100

    updated ogimage

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+        <meta name="description" content="Gabriel Temtsen | Fullstack Developer" key="desc" />
+        <meta property="og:title" content="Gabe Dev| Open Source Dev" />
+        <meta
+          property="og:description"
+          content="Gabriel Temtsen | Fullstack Developer"
+        />
+        <meta
+          property="og:image"
+          content="hhttps://avatars.githubusercontent.com/u/57184013?v=4"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
